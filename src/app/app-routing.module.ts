742ea6e75f9d645e90361e0b91da991f2bfb6dfe
JohnDev0000 <johncroft2236@hotmail.com>
2024-11-09 17:53:57 -0300

    reset head

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'alunos',
+    loadChildren: () => import('./tabs/alunos/alunos.module').then( m => m.AlunosPageModule)
+  },
 ];
 
 @NgModule({
